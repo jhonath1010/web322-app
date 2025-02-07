fa4678317727e7fd4d2a1870b6e037648fa338b5
Jhonatan Lopez <jhonatan@da-ich-bin.local>
2025-02-06 21:41:24 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+/*********************************************************************************
+*  WEB322 – Assignment 2
+*  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites, GPT) or distributed to other students.
+* 
+*  Name:        Jhonatan Lopez Olguin
+*  Student ID:  147028237
+*  Date:        6 Febr 2025
+*
+********************************************************************************/ 
